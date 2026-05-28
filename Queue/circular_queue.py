ee4a4e294e1cb8099ca7e07dbca3fc1ed7124b0e
Nothing4601 <heet5106@gmail.com>
2026-05-28 23:59:01 +0530

    Circular Queue

--- Queue/circular_queue.py
+++ Queue/circular_queue.py
@@ -1,1 +1,39 @@
+class Circular_queue:
+    def __init__(self,size):
+        self.size=size
+        self.cq=[size]
+        self.front=self.rear=-1
+
+    def cq_add(self,value): # at last
+        if self.front==-1:
+            self.front=self.rear=0
+            self.cq[self.front] = value
+        elif (self.rear+1)%self.size == self.front:
+            print('Circular Queue is full')
+        else:
+            self.rear=(self.rear+1)%self.size
+            self.cq[self.rear] = value
+        
+    def cq_delete(self):# from front
+        if self.front==-1:
+            print("Circular Queue is empty")
+        elif self.front==self.rear: # means both are 0 
+            print(f'{self.cq[self.front]} is deleted')
+            self.front=self.rear=-1
+        else:
+           self.front = (self.front + 1)%self.size
+
+a = Circular_queue(5)
+a.cq_add(1)
+a.cq_add(2)
+a.cq_add(3)
+a.cq_add(4)
+a.cq_add(5)
+a.cq_delete()
+a.cq_add(10)
+a.cq_add(15)
+a.cq_delete()
+
+
+
 
