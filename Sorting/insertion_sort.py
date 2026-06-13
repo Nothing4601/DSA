81947253358be74bd2e13f7325ba284cafab1c44
Nothing4601 <heet5106@gmail.com>
2026-06-13 23:58:03 +0530

    Commit insertion sort

---
+++ Sorting/insertion_sort.py
@@ -0,0 +1,9 @@
+def Insertion_sort(a):
+    n =len(a)
+    for i in range(n):
+        j=i
+        while (j!=0 and a[j]<a[j-1]):
+            a[j] , a[j-1] = a[j-1], a[j]
+            j-=1
+    return a
+print(Insertion_sort([3,2,5,23,18,20,3,5,6]))
